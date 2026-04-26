44d5ded4c0c563ac80bba54cc3cdbd70dbe636d0
Taewoong Ryu <49679902+FaExpCon@users.noreply.github.com>
2026-04-26 23:19:30 +0900

    [Silver I] Title: IOIOI, Time: 248 ms, Memory: 20108 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        String S = br.readLine();
+        
+        int count = 0;  
+        int result = 0; 
+    
+        for (int i = 1; i < M - 1; i++) {
+            if (S.charAt(i - 1) == 'I' && S.charAt(i) == 'O' && S.charAt(i + 1) == 'I') {
+                count++;
+                if (count >= N) {
+                    result++;
+                }
+                i++;
+            } else {
+                count = 0;
+            }
+        } 
+        System.out.println(result);
+    }
+}
